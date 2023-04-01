e83181cf7565c87c4ad3e6fd5b521ec125b86946
Luiz Paulo Valença <luizpaulo165@gmail.com>
2023-04-01 15:04:48 -0300

    Fix geral

---
+++ README.md.txt
@@ -0,0 +1,11 @@
+## Valuedot
+
+Introducing "Valuedot" - a Chrome extension that lets you easily convert images to grayscale right from your browser!
+
+With Valuedot, you can transform any image on the web into a black-and-white version with just a click of a button. Whether you want to view an image in a different light, or need a grayscale version for a specific purpose, this extension makes it simple and easy.
+
+Simply install the Valuedot extension, and when you come across an image you'd like to convert, just click on the extension icon in your browser. The image will instantly be transformed into grayscale. You can also toggle the effect on and off by clicking the icon again.
+
+Valuedot works seamlessly on all websites, from social media platforms to image search engines. It's the perfect tool for designers, photographers, or anyone who wants to experiment with different visual effects.
+
+Try Valuedot today and take your image browsing to the next level!
--- background.js
+++ background.js
@@ -1,16 +1,15 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.session.set({ checkStatus: false }, () => {
-    // console.log("checkStatus set to false");
+  chrome.storage.local.set({ checkStatus: false }, () => {
+    //
   });
 });
 
 chrome.action.onClicked.addListener(function (tab) {
-  chrome.storage.session.get("checkStatus", (data) => {
-    let ckeck = !data.checkStatus;
+  chrome.storage.local.get("checkStatus", (data) => {
+    let check = !data.checkStatus;
 
-    chrome.storage.session.set({ checkStatus: ckeck }, () => {
-      // console.log("checkStatus set to " + data.checkStatus);
-      handleTogglet();
+    chrome.storage.local.set({ checkStatus: check }, () => {
+      handleToggle();
     });
   });
 });
@@ -20,53 +19,34 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 
   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.url != currentUrl) {
-      handleTogglet();
-    } else {
-      console.log("bronken 2");
+      handleToggle();
     }
   });
 });
 
-function handleTogglet() {
+function handleToggle() {
   chrome.tabs
     .query({ active: true, currentWindow: true })
     .then(function (tabs) {
-      chrome.storage.session.get("checkStatus", (data) => {
-        // console.log(data.checkStatus);
+      chrome.storage.local.get("checkStatus", (data) => {
         if (data.checkStatus) {
-          chrome.action.setIcon({ path: "images/grayfy_active_32.png" });
+          chrome.action.setIcon({ path: "icons/grayfy_active_32.png" });
           chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
             func: function () {
-              // setTimeout(function () {
-              //   var imgs = document.querySelectorAll("img");
-              //   for (var i = 0; i < imgs.length; i++) {
-              //     imgs[i].style.filter =
-              //       "saturate(100%) grayscale(100%) contrast(100%) brightness(1)";
-              //     imgs[i].style.transition = "0.2s all ease-in-out";
-              //   }
-              // }, 500);
-              document.getElementsByTagName("html")[0].style.filter =
+              document.documentElement.style.filter =
                 "saturate(100%) grayscale(100%) contrast(100%) brightness(1)";
-              document.getElementsByTagName("html")[0].style.transition =
+              document.documentElement.style.transition =
                 "0.2s all ease-in-out";
             },
           });
         } else {
-          // console.log(data.checkStatus);
-          chrome.action.setIcon({ path: "images/grayfy_32.png" });
+          chrome.action.setIcon({ path: "icons/grayfy_32.png" });
           chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
             func: function () {
-              // var imgs = document.querySelectorAll("img");
-
-              // for (var i = 0; i < imgs.length; i++) {
-              //   console.log(imgs[i].length);
-              //   imgs[i].style.filter = "";
-              //   imgs[i].style.transition = "0.1s all linear";
-              // }
-              document.getElementsByTagName("html")[0].style.filter = "";
-              document.getElementsByTagName("html")[0].style.transition =
+              document.documentElement.style.filter = "";
+              document.documentElement.style.transition =
                 "0.2s all ease-in-out";
             },
           });
